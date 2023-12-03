b299f94b065f9110d04f2c32bab63cbecf51ace3
samuel-gwill <samuel.godswill2468@gmail.com>
2023-12-03 15:55:38 +0000

    2018 Day 9 (partial)

---
+++ 2018/solutions/day09-2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+int nextMarble(std::vector<int>& marbles) {
+	int next{ 1 };
+	while ( std::find(marbles.begin(), marbles.end(), next) != marbles.end() ) ++next;
+	return next;
+}
+
+int main() {
+	std::ifstream input{ "inputs/day09-input.txt" };
+	if (!input.is_open()) std::cout << "file did not open\n";
+	std::string s{};
+	std::getline(input, s);
+	
+	std::regex r{ "(\\d+).+?(\\d+)" };
+	std::smatch info;
+	std::regex_search(s, info, r);
+	
+	const int numPlayers { std::stoi(info[1]) };
+	const int maxMarbles { std::stoi(info[2]) };
+	std::vector<int> players(numPlayers, 0);
+	
+	std::list<int> marbles{ 0 };
+	int* curMarble{ &marbles[0] };
+	int curPlayer{ 0 };
+	int next { 1 };
+	while (next != maxMarbles + 1) {
+		if (next % 23 == 0) {
+			int score { next };
+			int removePos { static_cast<int>((curMarble + marbles.size() - 7) % marbles.size()) };
+			score += marbles[removePos];
+			marbles.erase(marbles.begin() + removePos);
+			curMarble = removePos;
+			players[curPlayer] += score;
+		} else {
+			
+		}
+		
+		++next;
+		++curPlayer;
+		curPlayer %= numPlayers;
+	}
+	
+	std::cout << *(std::max_element(players.begin(), players.end())) << '\n';
+}
