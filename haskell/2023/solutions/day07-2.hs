2afcf1cf443d5ee1cace7aedf98046192bf8d276
samuel-gwill <samuel.godswill2468@gmail.com>
2024-11-01 19:00:06 +0000

    Day 7 Haskell

---
+++ haskell/2023/solutions/day07-2.hs
@@ -0,0 +1,56 @@
+import Data.List
+
+data Card = J | Two | Three | Four | Five | Six | Seven | Eight | Nine | T | Q | K | A
+   deriving (Eq, Ord, Show, Read)
+
+toCard :: Char -> Card
+toCard c = case c of
+           '2' -> Two
+           '3' -> Three
+           '4' -> Four
+           '5' -> Five
+           '6' -> Six
+           '7' -> Seven
+           '8' -> Eight
+           '9' -> Nine
+           'T' -> T
+           'J' -> J
+           'K' -> K
+           'Q' -> Q
+           'A' -> A
+           _   -> Two
+
+handStrength :: String -> Int
+handStrength hand = let jokers = length $ filter (=='J') hand
+                        noJokers = reverse $ sort $ map length (group $ sort $ filter (/='J') hand)
+                        handType = if jokers == 5 then [5] else (jokers + head noJokers) : tail noJokers
+                    in case handType of
+                       [5]             -> 8
+                       [4, 1]          -> 7
+                       [3, 2]          -> 6
+                       [3, 1, 1]       -> 5
+                       [2, 2, 1]       -> 4
+                       [2, 1, 1, 1]    -> 3
+                       [1, 1, 1, 1, 1] -> 2
+                       _               -> 1
+
+compareHand :: String -> String -> Ordering
+compareHand hand1 hand2 = if order /= EQ then order else handCheck hand1 hand2 0
+   where order = compare (handStrength hand1) (handStrength hand2)
+         handCheck h1 h2 i
+            | i >= 5      = EQ
+            | order /= EQ = order
+            | otherwise   = handCheck h1 h2 (i + 1)
+            where order = compare (toCard (h1 !! i)) (toCard (h2 !! i))
+
+readLine :: String -> (String, Int) 
+readLine s = let w = words s
+             in (w !! 0, read $ w !! 1)
+
+solution :: String -> Int
+solution s = let bids = sortBy (\a b -> compareHand (fst a) (fst b)) (map readLine (lines s))
+             in sum $ map (\(rank,(_,bid)) -> rank * bid) (zip [1..] bids)
+
+main = do
+       file <- readFile "../inputs/2023/day07-input.txt"
+       putStrLn $ show $ solution file
